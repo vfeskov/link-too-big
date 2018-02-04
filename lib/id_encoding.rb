1568fe015fd39009c742d5773551bd79fc6a14b0
Vladimir Feskov <v@vladimirfeskov.com>
2018-02-04 18:17:34 +0100

    refactor(expander/ruby): simplify number system characters

--- lib/id_encoding.rb
+++ lib/id_encoding.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 class IdEncoding
   # Number System
-  NS = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f g h i j k l m n o p q r s t u v w x y
-          z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z _ -).freeze
+  NS = ([*'0'..'9'] + [*'a'..'z'] + [*'A'..'Z'] + %w(- _)).freeze
   NS_BASE = NS.length
 
   def self.encode(id)
