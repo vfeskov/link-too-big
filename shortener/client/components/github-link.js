dc9e5d11be058feb5db3cb56d8dfff2415da1032
Vladimir Feskov <v@vladimirfeskov.com>
2018-02-05 01:42:21 +0100

    fix(shortener/client): visibility of the new star button

--- shortener/client/components/github-link.js
+++ shortener/client/components/github-link.js
@@ -11,7 +11,7 @@ Vue.component('github-link', {
 
   render() {
     return (
-      <div class="github-link-wrapper" style={this.hide && 'text-indent: -9999px;'}>
+      <div class="github-link-wrapper" style={this.hide && 'opacity:0;'}>
         <a class="github-button" href="https://github.com/vfeskov/link-too-big" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star vfeskov/link-too-big on GitHub">Star</a>
       </div>
     )
