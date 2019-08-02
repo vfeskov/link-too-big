d8b588a745bf70ba23319c411506fde4f87bd6ab
Vlad Feskov <v@vladimirfeskov.com>
2019-08-02 12:16:21 +0300

    Use INCR operation to get next id

--- shortener/server/db.js
+++ shortener/server/db.js
@@ -1,32 +1,16 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
 const { REDIS_URL } = process.env;
-const LASTID = 'lastid';
+const LASTID_KEY = 'lastid';
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 const db = redis.createClient(REDIS_URL);
 
 module.exports = {
   async insert (link, random) {
-    let id = await this.nextId();
-    await db.multi()
-      .set(id, random + link)
-      .set(LASTID, id)
-      .execAsync();
+    const id = await db.incrAsync(LASTID_KEY);
+    await db.setAsync(id, random + link);
     return id;
-  },
-
-  async nextId () {
-    let id = +await db.getAsync(LASTID) + 1;
-    if (isNaN(id) || id <= 0) { id = 1; }
-    while (await this.taken(id)) { id++; }
-    return id;
-  },
-
-  async taken (id) {
-    const taken = await db.getAsync(id)
-    return !!taken;
   }
 }
